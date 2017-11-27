48ba1e4a1ae18a9a806586255312d896b6e71306
MahshidHosseini <33192812+MahshidHosseini@users.noreply.github.com>
2017-11-27 20:07:12 +0330

    Update Form1.cs

--- GetPersianNameOfAirports/GetPersianNameOfAirports/Form1.cs
+++ GetPersianNameOfAirports/GetPersianNameOfAirports/Form1.cs
@@ -64,7 +64,7 @@ namespace GetPersianNameOfAirports
             }
 
             MessageBox.Show("End");
-            MessageBox.Show("Test Git");
+            MessageBox.Show("Merge Conflict");
 
         }
     }
